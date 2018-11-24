112141a3c5c6d416c996caa38c8337b881cc1802
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-11-24 15:56:34 -0500

    finished 2 steps of set up and converted to chrome storage

--- js/updates.js
+++ js/updates.js
@@ -1,27 +1,35 @@
 // Uncomment the below line to restart the signing up process
 // Make sure to recomment it and reload the page before proceeding
 
-localStorage.clear()
+//  localStorage.clear()
+// chrome.storage.sync.clear()
 
-// Update Functions
+// UPDATE FUNCTIONS
+
 window.onload = update_page()
 
 function update_page() {
+  updateCover()
   if (window.location.pathname == "/start.html") {
     setUpStartPage()
     return
   }
-  if (first_time()) { setUp() }
-  else {
+  else if (first_time()) { setUp() }
+  else if (window.location.pathname == "/newTab.html") {
     update_badges()
     update_copy()
   }
 }
 
+// SET UP FUNCTIONS
+
 // Check if this is their first time using the app
 
 function first_time() {
-  var name = localStorage.getItem("name")
+  let result = chrome.storage.sync.get(['name'], function(result) {
+    console.log('Name is currently' + result.name)
+  })
+  var name = result.name
   if (name == null || name == "") { return true }
   return false
 }
@@ -32,23 +40,47 @@ function setUp() {
   window.location = "start.html"
 }
 
+// If first time, then set up everything
+
+function inSetUp() {
+  setupPages = ["/start.html", "/start_accounts.html"]
+  for (i=0; i < setupPages.length; i++) {
+    if (window.location.pathname == setupPages[i]) {
+      return true
+    }
+  }
+  return false
+}
+
 function setUpStartPage() {
   document.getElementsByClassName("nameInput")[0].focus()
   document.getElementsByClassName("nameSaveBtn")[0].addEventListener('click', saveName)
-  document.body.setAttribute("style", `
-    background: url(https://source.unsplash.com/bF2vsubyHcQ) no-repeat center center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-  `)
 }
 
 function saveName() {
   var name = document.getElementsByClassName("nameInput")[0].value
-  localStorage.setItem("name", name)
-  window.location = "newTab.html"
+  // localStorage.setItem("name", name)
+  chrome.storage.sync.set({'name':name}, function() { //saves the name to the cloud
+    console.log('Name saved: ' + name);
+  });
+  if (inSetUp()) {
+    window.location = "start_accounts.html"
+  }
 }
+
+function updateCover() {
+  if (inSetUp()) {
+    document.body.setAttribute("style", `
+      background: url(https://source.unsplash.com/bF2vsubyHcQ) no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    `)
+  }
+}
+
+// NORMAL PAGE UPDATES (post set up)
 
 // Update number of unread messages
 
