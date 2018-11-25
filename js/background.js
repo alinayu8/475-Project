544cb73be82813cfeba3f8ac222fccb0cf862700
Alina Yu <alinayu8@yahoo.com>
2018-11-24 22:29:27 -0500

    Able to access any Slack workspace. Able to login to multiple Slack workspaces.

--- js/background.js
+++ js/background.js
@@ -4,22 +4,22 @@
 
 'use strict';
 
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-	console.log("Background JS responding");
-	console.log(response);
-	saveToken(response);
-});
+// chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+// 	console.log("Background JS responding");
+// 	console.log(response);
+//         saveToken(response);
+// });
 
-//copied save function from popup js
-function saveToken(retrievedToken) {
-        // Get a value saved in a form.
-        var token = retrievedToken;
-        // Check that there's some code there.
-                // Save it using the Chrome extension storage API.
-        chrome.storage.sync.set({userToken: token}, function() {
-          // Notify that we saved.
-          console.log('Settings saved:');
-		  console.log(token);
-		  tokenforcalls = token; //updates global token with new value from authing.
-        });
-      }
+// //copied save function from popup js
+// function saveToken(retrievedToken) {
+//     // Get a value saved in a form.
+//     var token = retrievedToken;
+//     // Check that there's some code there.
+//             // Save it using the Chrome extension storage API.
+//     chrome.storage.sync.set({userToken: token}, function() {
+//         // Notify that we saved.
+//         console.log('Settings saved:');
+//         console.log(token);
+//         tokenforcalls = token; //updates global token with new value from authing.
+//     });
+// }
