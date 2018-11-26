6d4c08aa4e83dcba95b9a06b9a131445f99e57eb
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-11-26 12:39:41 -0500

    clearning local storage line added

--- js/saveconvos.js
+++ js/saveconvos.js
@@ -14,7 +14,8 @@ $(window).on("unload", function(e) {
 });
 
 //UNCOMMENT AND RELOAD SCREEN ONCE TO WIPE OUT ALL CHROME STORAGE DATA
-$(window).on("load", function () {
-    chrome.storage.local.clear();
-    chrome.storage.sync.clear();
-});
+// $(window).on("load", function () {
+//     chrome.storage.local.clear();
+//     chrome.storage.sync.clear();
+//     localStorage.clear();
+// });
